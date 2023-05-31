090b6c1367de7711cf3dcefa40ca7df0bfa78be5
David Slusser <dbslusser@gmail.com>
2023-05-30 20:06:28 -0700

    testing radon

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,12 @@ time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
 python --version
-"radon cc -a src"
 
 OUTPUT=$(radon cc -a src | tail -1)
 GRADE=$(echo $OUTPUT | grep -oP " \w " | tr -d '[:space:]')
 rc=1
+
+echo "grade is $GRADE"
 
 if [[ $GRADE == "B" || $GRADE < "B" ]] ; then  rc=0 ; fi
 
