897c04e06e42b377caa38feb4b1534b1a12fe128
David Slusser <dbslusser@gmail.com>
2023-05-30 19:54:17 -0700

    testing radon

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ python --version
 ls -l
 
 OUTPUT=$(radon cc -a src | tail -1)
-GRADE=$(echo $OUTPUT | grep -oP " \w " | tr -d '[:space:]')
-
+#GRADE=$(echo $OUTPUT | grep -oP " \w " | tr -d '[:space:]')
+GRADE = "C"
 rc=1
 
 if [[ $GRADE == "B" || $GRADE < "B" ]] ; then  rc=0 ; fi
