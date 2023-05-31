379f56800fcf9a7675bbb178ec2abeb44505c14f
David Slusser <dbslusser@gmail.com>
2023-05-30 21:28:42 -0700

    testing radon

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 python --version
-OUTPUT=$(radon cc -a -s -O radon.out $1)
+OUTPUT=$(radon cc -a -s -n$2 -O radon.out $1)
 cat radon.out
 GRADE=$(cat radon.out | tail -1 | sed 's/.*: \([^ ]*\).*/\1/')
 rc=1
-if [[ $GRADE == $2 || $GRADE < $2 ]] ; then  rc=0 ; fi
+if [[ $GRADE == $3 || $GRADE < $3 ]] ; then  rc=0 ; fi
 exit $rc
