e09e2b63a8e364ec525fd222e144ce8718dfc5e5
David Slusser <dbslusser@gmail.com>
2023-05-30 20:41:45 -0700

    testing radon

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ python --version
 
 pylint $1 --exit-zero
 
-OUTPUT=$(radon cc -a $1 | tail -1)
+OUTPUT=$(radon cc -a $1 | tee /dev/tty | tail -1)
 echo $OUTPUT
 GRADE=$(echo $OUTPUT | grep -oP " \w " | tr -d '[:space:]')
 rc=1
 
-echo "grade is $GRADE"
+# echo "grade is $GRADE"
 
 if [[ $GRADE == "B" || $GRADE < "B" ]] ; then  rc=0 ; fi
 
