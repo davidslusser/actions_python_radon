980c5a05ddc7302521f75794b1a7559adcd92c48
David Slusser <dbslusser@gmail.com>
2023-05-30 21:43:01 -0700

    finalizing action and entrypoint; updating readme

--- entrypoint copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-python --version
-OUTPUT=$(radon cc -a -s $1 | tee /dev/stderr | tail -1)
-GRADE=$(echo $OUTPUT | grep -oP " \w " | tr -d '[:space:]')
-rc=1
-if [[ $GRADE == $2 || $GRADE < $2 ]] ; then  rc=0 ; fi
-exit $rc
